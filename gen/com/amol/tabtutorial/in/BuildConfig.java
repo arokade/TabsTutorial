a1564cf9e08135c2d3c8764f5015b9a9a8213ea7
arokade <amolrokade@Amols-MacBook-Air.local>
2015-03-29 12:41:18 +0530

    add multiple tab

---
+++ gen/com/amol/tabtutorial/in/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.amol.tabtutorial.in;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/androidbegin/fragmenttabstutorial/FragmentTab1.java
@@ -0,0 +1,18 @@
+package com.androidbegin.fragmenttabstutorial;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+import com.amol.tabtutorial.in.R;
+
+public class FragmentTab1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragmenttab1, container, false);
+        return rootView;
+    }
+ 
+}
